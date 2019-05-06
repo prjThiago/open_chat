6d0fa2abf4a4378e4138aeffda0777099ab567af
Thiago Morais <thiagomorais@projetosthiago.com.br>
2019-05-06 07:57:05 -0300

    commit config io-part1

---
+++ src/api/controllers/chat/chatService.js
@@ -0,0 +1,33 @@
+const IO = (app, urlPath) => {
+  const io = require('socket.io').listen(app)
+
+  io.on('connection', socket => {
+    console.log('conectou')
+
+    socket.on('disconect', () => {
+      console.log('Usuário desconectoun')
+    })
+
+    socket.on(urlPath + 'MsgToServer', data => {
+      socket.emit('msgToClient', {
+        nickname: data.nickname,
+        message: data.message
+      })
+
+      socket.broadcast.emit(urlPath + 'msgToClient', {
+        nickname: data.nickname,
+        message: data.message
+      })
+
+      if (parseInt(data.refresh_members) === 0) {
+        socket.emit(urlPath + 'membersToClient', { apelido: data.apelido })
+
+        socket.broadcast.emit(urlPath + 'membersToClient', {
+          apelido: data.apelido
+        })
+      }
+    })
+  })
+}
+
+module.exports = IO
--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 const app = require('./config/server')
-const chats = []
+const _ = require('lodash')
+const chats = {}
 
 app.listen(3000, () => {
   console.log('Server is running on 3000')
+})
+
+app.get('/', (req, res) => {
+  res.status(200).json({
+    chats
+  })
+})
+
+app.use((req, res, next) => {
+  let urlPath = req.path
+
+  // Configurando as salas.
+  if (_.has(chats, urlPath)) {
+  } else {
+    chats[urlPath] = require('./api/controllers/chat/chatService')(app, urlPath)
+  }
 })
